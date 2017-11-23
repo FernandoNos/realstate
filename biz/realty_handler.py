ba87813f7ea46d6c04e044f72661469923ed6658
Fernando Nos <fernando461@gmail.com>
2017-11-23 00:27:27 -0200

    updated to not run the update flow during initialization

--- biz/realty_handler.py
+++ biz/realty_handler.py
@@ -3,8 +3,10 @@ import json
 
 
 sys.path.append('dao')
+sys.path.append('utils')
 from time import gmtime, strftime
 
+import custom_logger
 import realty_api
 import data_handler
 
@@ -13,25 +15,23 @@ def getRealtyCodes(page):
 
 def updateRealty(code,obsrv):
 	new_obsrv= data_handler.removeDate(obsrv)
-	return realty_api.updateRealty(code,new_obsrv+' \n'+strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+	return realty_api.updateRealty(code,new_obsrv+' \n'+strftime("%Y-%m-%d", gmtime()))
 
 def updateRealties():
 	page = 1
 	pages = -1
-	while page != pages:
+	response = []
+	while page!=pages+1:
 		resp = getRealtyCodes(page)
 		pages = int(resp[0][1])
 		page = int(resp[1][1])
-		print str(pages) + ' ' + str(page)
 		realty_codes = resp[2][1]
-		response = []
 		for elem in realty_codes:
 			if str(elem)=='':
 				continue
 			realty = realty_api.getRealtyDetail(elem)
+			custom_logger.logRealty(elem,realty[1],page)
 			resp = updateRealty(realty[0],realty[1])
 			response.append({'Codigo':resp['Codigo'],'Mensagem':resp['message']})
 		page = page + 1
 	return str(response)
-
-updateRealties()
---
+++ utils/custom_logger.py
@@ -0,0 +1,21 @@
+import os
+from time import gmtime, strftime
+
+def logRealty(code, obsrv, page):
+	createFolder(page)
+	upsertFile(page,code,obsrv)
+
+def createFolder(page):
+	date_folder = strftime("%Y-%m-%d", gmtime())
+	page_file = '/page_'+str(page)
+	if not os.path.exists('log'):
+		os.makedirs('log')
+	if not os.path.exists('log/'+date_folder):
+		os.makedirs('log/'+date_folder)
+
+def upsertFile(page,code,obsrv):
+	f=open('log/'+strftime("%Y-%m-%d", gmtime())+'/page_'+str(page)+'.log', 'a+')
+	f.write(str(code)+' - Descricao antiga:'+obsrv)
+	f.write('\n--------------------------------------------------------------------------\n')
+	f.close()
+
