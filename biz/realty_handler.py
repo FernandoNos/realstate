f055b5c86b4434acbbfd185a8a280cbb0dab2ba9
Fernando Nos <fernando461@gmail.com>
2017-12-05 21:09:02 -0200

    testing

--- biz/realty_handler.py
+++ biz/realty_handler.py
@@ -41,6 +41,7 @@ def updateRealties():
 				realty = realty_api.getRealtyDetail(elem)
 				custom_logger.logRealty(elem,realty[1],page)
 				resp = updateRealty(realty[0],realty[1])
+				print ('This is the response '+str(resp)).encode('utf-8')
 				response.append({'Codigo':resp['Codigo'],'Mensagem':resp['message']})
 				print response
 			page = page + 1
