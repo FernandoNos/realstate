45d324fb4363515a1fb503608ac452327b772073
Fernando Nos <fernando461@gmail.com>
2017-12-05 22:59:13 -0200

    filtering by agencia

--- dao/realty_dao.py
+++ dao/realty_dao.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import sys,os
+import logging 
 
 sys.path.append('utils')
 import initializer
@@ -13,7 +14,7 @@ def listRealties(fields,page):
 	headers = {'Accept': 'application/json', 'Content-type': 'application/json'}
 
 	#Lists all the realties
-	pesquisa='{"fields": ["'+fields+'"],"paginacao":{"pagina":'+str(page)+',"quantidade":50}}'
+	pesquisa='{"fields": ["'+fields+'"],"filter":{"ImobiliariaCadastro":"PLANEJAR ASSESSORIA IMOBILIARIA"},"paginacao":{"pagina":'+str(page)+',"quantidade":50}}'
 	url = URL+'/imoveis/listar?key='+KEY+'&showtotal=1&pesquisa='+pesquisa
 
 	response = requests.get(url,headers=headers)
@@ -32,6 +33,7 @@ def updateRealty(code, obsrv):
 	headers = {'Accept': 'application/json', 'Content-type': 'application/x-www-form-urlencoded'}
 	url = URL+'/imoveis/detalhes?key='+KEY+'&imovel='+code
 	cadastro='cadastro={"fields":{"Descricao":"'+obsrv+'"}}'
+	logging.info('cadastr='+str(cadastro))
 	response = requests.put(url, headers=headers,data=cadastro)
 	return response.text
 
