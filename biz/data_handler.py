f7948d0b9df929769ea01650f068da0f0b4391e3
Fernando Nos <fernando461@gmail.com>
2018-02-27 22:43:52 -0300

    updating the string format

--- biz/data_handler.py
+++ biz/data_handler.py
@@ -1,5 +1,5 @@
 import re
 
 def removeDate(obsrv):
-	return  re.sub(u'\n[0-9][0-9][0-9][0-9]\-[0-9][0-9]\-[0-9][0-9]$', u'',obsrv).replace('"','\\"').replace('\\\\\"','\\\"').replace('\'','\\\'').replace('&#039;','\'').encode('utf-8')
+	return  re.sub(u'\n(Ultima atualizacao:)?[0-9][0-9][0-9][0-9]\-[0-9][0-9]\-[0-9][0-9]$', u'',obsrv).replace('"','\\"').replace('\\\\\"','\\\"').replace('\'','\\\'').replace('&#039;','\'').encode('utf-8')
 	
